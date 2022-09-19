406ae17ea660027efe7331113ec0a72c1963f662
h2244r <84302269+h2244r@users.noreply.github.com>
2022-09-19 08:33:35 +0530

    Update GP-NEW.js

--- GP-NEW.js
+++ GP-NEW.js
@@ -1,6 +1,6 @@
 var script = document.createElement('script');
 var head = document.getElementsByTagName("head")[0];
-var A1 = document.createTextNode(`function shuffle(h){for(let t=h.length-1;0<t;t--){var a=Math.floor(Math.random()*(t+1)),f=h[t];h[t]=h[a],h[a]=f}return h}const path=["aeiou","cHUZzyp","NMjwbwYz","QQeZG","piaYt9Y","qlV1nv","M9o5hUC7","JgC4Do","mo9JGG","RQbU1","6HfaY","UBqv","F0iMZ","I0s4Y8u","Dni79zD","f9bCF","Z2g7yBVF","jHEt3drS","4OYS","4j4GjU","EO32dnm","8e4uGSVg","OfrqY","E12KWs6t","Whsceb1D","DV1wd5A","sDCyZ","3k6kT","1VulBwOs","N7OP","zeUO8Hpf","8glqEpvv","QAyQch0P","Fq51oI2","K93ur","KoKCo","Oe4tH","WBY1si","LUN2vbs","ZC6Bx","Jt60yyad","sFjf","B2xbXNR","VtOc","hCuzku","6q11sSyq","V0Rqavh3","Vqthfdy","stuMJ","I8p0xnU","mh5oKA2","ekXjK","T4k3swE","sdG00","3tYTEG","RjmqKWV","lPozhn","LaDI","ylCpDw0R","lZoB2","oua6","A5ZXTy","buOSKf3x","5SyQwP","31ekWm","7YwTR8","OMF6S9"],pathshuffle=shuffle(path),random=pathshuffle[Math.floor(Math.random()*pathshuffle.length)];window.location="https://gplinks.co/"+random;`);
+var A1 = document.createTextNode(`function shuffle(h){for(let t=h.length-1;0<t;t--){var a=Math.floor(Math.random()*(t+1)),f=h[t];h[t]=h[a],h[a]=f}return h}const path=["9lgCF","I1cX5","7v9z","rPUwOrT3","DgxHryE","ZgbNby9W","IkCpUkXE","6PKs","bhBSQ","QNUpG","mVSL","HFtj0S1","yeCcdu","11FL","l3T5CL9F","uTMH7Eh","ftw0F","H73KYPd","x10zjlrR","qypRG2aA","bGCGo","cmnn","FgHr8f7","znAjn","0aWMss4","Qo8CDT","G2YfTF","CrqeDGH4","NLGVK7","gY7wc4Xy","PqH66K","WWyZu","FztF","Lki5td","SpgpCHlF","0DF1","u34eminS","FSM5Fuz","h1iPeyf","B0n3zu","K7V0xKfh","rHr5NV","aeiou","cHUZzyp","NMjwbwYz","QQeZG","piaYt9Y","qlV1nv","M9o5hUC7","JgC4Do","mo9JGG","RQbU1","6HfaY","UBqv","F0iMZ","I0s4Y8u","Dni79zD","f9bCF","Z2g7yBVF","jHEt3drS","4OYS","4j4GjU","EO32dnm","8e4uGSVg","OfrqY","E12KWs6t","Whsceb1D","DV1wd5A","sDCyZ","3k6kT","1VulBwOs","N7OP","zeUO8Hpf","8glqEpvv","QAyQch0P","Fq51oI2","K93ur","KoKCo","Oe4tH","WBY1si","LUN2vbs","ZC6Bx","Jt60yyad","sFjf","B2xbXNR","VtOc","hCuzku","6q11sSyq","V0Rqavh3","Vqthfdy","stuMJ","I8p0xnU","mh5oKA2","ekXjK","T4k3swE","sdG00","3tYTEG","RjmqKWV","lPozhn","LaDI","ylCpDw0R","lZoB2","oua6","A5ZXTy","buOSKf3x","5SyQwP","31ekWm","7YwTR8","OMF6S9","965N","xmInJ","ZaIN2Cx","eB9Lcloi","V2iHHomw","fkxhTG7W","VPVClE9Y","Xf7fZYm"],pathshuffle=shuffle(path),random=pathshuffle[Math.floor(Math.random()*pathshuffle.length)];window.location="https://gplinks.co/"+random;`);
 
 function ref() {
     const i = setInterval(() => {
@@ -129,6 +129,18 @@ setTimeout(() => {
         if (window.location.host == 'gplinks.in') {
             window.location = "https://h2244r.github.io/gp/";
         }
+        if (document.querySelector("#mh-mobile")) {
+            window.location = "https://h2244r.github.io/gp/";
+        }
+        if (document.querySelector('body[id="mh-mobile"]')) {
+            window.location = "https://h2244r.github.io/gp/";
+        }
+        if (document.querySelector("#mh-mobile > pre").innerText == 'Array\n(\n)\n') {
+            window.location = "https://h2244r.github.io/gp/";
+        }
+        if (document.querySelector('div[class="mh-header-text"] a[class="mh-header-text-link"][title="MYNEWSMEDIA"][rel="home"]')) {
+            window.location = "https://h2244r.github.io/gp/";
+        }
     }, 2000);
     setInterval(() => {
         if (document.querySelector("body > nav")) {
@@ -159,15 +171,10 @@ setTimeout(() => {
             });
         }
     }, 50);
-}, 1000);
+}, 1500);
 
 setTimeout(() => {
     if (window.location.host == 'mynewsmedia.co' && document.querySelector("#btn6")) {
-        setInterval(() => {
-            if (document.querySelector('body[id="mh-mobile"]')) {
-                window.location = "https://h2244r.github.io/gp/";
-            }
-        }, 2000);
         adclick();
         setTimeout(() => {
             window.scrollTo({
